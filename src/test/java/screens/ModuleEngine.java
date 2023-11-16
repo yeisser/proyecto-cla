cdcf6960f7ae50b12c2849735f127edc1ed82693
yeisser <@yaguilera>
2023-11-16 02:43:22 -0500

    Actualizando proyecto version 2

---
+++ src/test/java/screens/ModuleEngine.java
@@ -0,0 +1,12 @@
+package screens;
+
+import task.Campaings_Task;
+import task.Engine_Task;
+import utility.UIOperation;
+
+public class ModuleEngine {
+    public void click_iniciar_sesion() {
+        UIOperation.giveClick(Engine_Task.btn_iniciar_sesion_engine);
+    }
+
+}
---
+++ src/test/java/stepDefinations/ModuleEngine.java
@@ -0,0 +1,26 @@
+package stepDefinations;
+
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Given;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import screens.recoveries_screens;
+
+public class ModuleEngine {
+    recoveries_screens gestionmora = new recoveries_screens();
+    screens.ModuleEngine engine = new screens.ModuleEngine();
+    WebDriver driver;
+    @Given("^el usuario se conecta a kata engine e ingresa al login$")
+    public void conexion_enginee_login() {
+        System.setProperty("webdriver.chrome.driver","src//test//resources//drivers//chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.navigate().to("https://qa.andes.engine.kata-software.com/login");
+        gestionmora.espera_un_min();
+    }
+    @When("^el usuario ingresa al engine y hago onbording con \"([^\"]*)\" y \"([^\"]*)\"$")
+    public void usuario_ingresa_engine(String user, String pass) {
+        engine.click_iniciar_sesion();
+    }
+}
+
